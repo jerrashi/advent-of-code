a55c065ca86b80771cd5c8911f43385b004e8590
Jerry Shi <40506087+jerrashi@users.noreply.github.com>
2023-12-06 18:09:27 -0500

    finished part 1 of day 3 advent of code

--- day-3/day3-part1.py
+++ day-3/day3-part1.py
@@ -1,10 +1,35 @@
-import re
-
 with open("input.txt", 'r') as file:
     file_2d_array = []
-    i = 0
     for line in file:
-        print(line.strip())  # strip() removes newline characters at the end
         row = [char for char in line.strip()]  # List comprehension for each character
         file_2d_array.append(row)  # Append the row to the 2d array
-    print(file_2d_array)
+
+directions = [(-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0)]
+res = 0
+
+for i in range(len(file_2d_array)):
+    for j in range(len(file_2d_array[0])):
+        symbol = file_2d_array[i][j]
+        if not symbol.isdigit() and symbol != ".":  # symbol is not a period or number
+            # search all directions
+            for direction in directions:
+                new_i, new_j = i + direction[0], j + direction[1]
+                if 0 <= new_i < len(file_2d_array) and 0 <= new_j < len(file_2d_array[0]):
+                    if file_2d_array[new_i][new_j].isdigit():
+                        current_number = file_2d_array[new_i][new_j]
+                        file_2d_array[new_i][new_j] = '.'
+                        # prepend all the left digits
+                        scan = -1
+                        while 0 <= new_j + scan and file_2d_array[new_i][new_j + scan].isdigit():
+                            current_number = file_2d_array[new_i][new_j + scan] + current_number
+                            file_2d_array[new_i][new_j + scan] = '.'
+                            scan -= 1
+                        # append all the right digits
+                        scan = 1
+                        while new_j + scan < len(file_2d_array[0]) and file_2d_array[new_i][new_j + scan].isdigit():
+                            current_number += file_2d_array[new_i][new_j + scan]
+                            file_2d_array[new_i][new_j + scan] = '.'
+                            scan += 1
+                        res += int(current_number)
+
+print(res)
